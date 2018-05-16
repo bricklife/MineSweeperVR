aad8470e81b04400002120f3681c8773697cd097
Shinichiro Oba <ooba@bricklife.com>
2018-05-17 00:12:22 +0100

    Show cubes and bombs by script

--- Assets/ChangeColor.cs
+++ Assets/ChangeColor.cs
@@ -7,38 +7,42 @@ public class ChangeColor : MonoBehaviour
 {
     [SerializeField] private VRInteractiveItem m_InteractiveItem;
     public bool isGazeOver;
-    
+
     private void OnEnable()
     {
         m_InteractiveItem.OnOver += HandleOver;
         m_InteractiveItem.OnOut += HandleOut;
-        m_InteractiveItem.OnUp += HandleOver;
         m_InteractiveItem.OnDown += HandleClick;
+        m_InteractiveItem.OnUp += HandleUp;
     }
-    
+
     private void OnDisable()
     {
-        m_InteractiveItem.OnOver -= HandleOver;
+        m_InteractiveItem.OnUp -= HandleUp;
         m_InteractiveItem.OnOut -= HandleOut;
-        m_InteractiveItem.OnUp -= HandleOver;
         m_InteractiveItem.OnDown -= HandleClick;
+        m_InteractiveItem.OnUp -= HandleUp;
     }
-    
+
     private void HandleOver()
     {
         isGazeOver = true;
         gameObject.GetComponent<Renderer>().material.color = Color.green;
     }
-    
+
     private void HandleOut()
     {
         isGazeOver = false;
         gameObject.GetComponent<Renderer>().material.color = Color.white;
     }
-    
+
     private void HandleClick()
     {
-        isGazeOver = true;
         gameObject.GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    private void HandleUp()
+    {
+        Destroy(gameObject);
     }
 }
---
+++ Assets/Field.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Field : MonoBehaviour
+{
+
+    public GameObject cube;
+    public GameObject bomb;
+
+    // Use this for initialization
+    void Start()
+    {
+        var center = new Vector3(0f, 0, 0);
+        var num = 90;
+        var r = 10;
+        var size = Mathf.PI * r * 2 / num;
+        var angle = 360 / num;
+        var scale = 0.98f;
+
+        for (int y = -10; y <= 10; y++)
+        {
+            for (int i = 0; i < num; i++)
+            {
+
+                foreach (var item in new GameObject[] {cube, bomb})
+                {
+                    var obj = Instantiate(item, new Vector3(0f, y * size, r + size / 2), Quaternion.identity);
+                    obj.transform.RotateAround(center, Vector3.up, angle * i);
+                    obj.transform.localScale = new Vector3(size * scale, size * scale, size * scale);
+                    obj.transform.parent = transform;
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //var rotY = Quaternion.AngleAxis(45.0f * Time.deltaTime, Vector3.up);
+        //transform.rotation *= rotY;
+    }
+}
